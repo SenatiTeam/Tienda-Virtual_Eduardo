4342fcaaa4cd0bc7436fef91c6a89747f30bfd7d
Lalo2610 <148415473+Lalo2610@users.noreply.github.com>
2023-11-16 10:23:11 -0500

    ultimo
    ultimo

---
+++ conexBD_usuarioRegistro.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("conexion.php");
+
+//Variables
+$nombre_usuario =$_POST['nombreU'];
+$correo_usuario =$_POST['correoU'];
+$clave_usuario =$_POST['claveU'];
+$roles_usuario =$_POST['rolesU'];
+
+/* Mensaje */
+//echo $nombre_usuario.'___'.$correo_usuario. "<br>";
+//echo $clave_usuario.'___'.$roles_usuario;
+
+/* Codigo SQL - mediante script */
+$sql = "INSERT INTO usuario(name_user,email_user,password_user,roles_user) VALUES('$nombre_usuario','$correo_usuario','$clave_usuario','$roles_usuario')";
+
+$resultado = $conexion->query($sql) or die($conexion->connect_error);
+if ($resultado){
+    echo"Registro importado exitosamente";
+}else{
+    echo "Error".$conexion->error;
+}
+
+//Cerramos la conexion, siempre utilizamos esto cada vez que usamos un registro
+$conexion -> close();
+
+?>
--- index.php
+++ index.php
@@ -21,41 +21,107 @@ if(isset($_SESSION['datos'])){
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
+    <!-- ....... MENU ....... -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">TIENDA VIRTUAL - <?php echo $nombre; ?></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="cerrarSesion.php"><img width="96" height="96" src="https://img.icons8.com/color/96/shutdown--v1.png" alt="shutdown--v1"/></a>
-            </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-            </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-        <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">TIENDA VIRTUAL - <?php echo $nombre; ?></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cerrarSesion.php"><img width="26" height="26" src="https://img.icons8.com/color/96/shutdown--v1.png" alt="shutdown--v1"/></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+    <!-- ....... PIMCIPAL ....... -->
+    <div class="container my-5">
+        <div class="row">
+            <!--Aqui va ir el formulario de registro de usuario-->
+            <div class="col-md-4">
+                <h2>REGISTRO DE USUARIO</h2>
+                <form action="conexBD_usuarioRegistro.php" method="POST">
+                    <div class="mb-3">
+                        <label for="exampleInputNombre" class="form-label">Nombre</label>
+                        <input type="text" name="nombreU" placeholder="Ingrese el nombre de usuario" class="form-control" id="exampleInputNombre" aria-describedby="nombreHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
+                        <input type="email" name="correoU" placeholder="Ingrese su correo" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"> <!--placeholder: es para poner el escrito que desaparece al escribir-->
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                        <input type="password" name="claveU" placeholder="Ingrese su contraseña" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputRoles" class="form-label">Roles</label>
+                        <select name="rolesU" name="rolesU" id="exampleInputRoles" class="form-select" aria-label="Default select example">
+                            <option selected>Seleccione su rol</option>
+                            <option value="administrador">Administrador</option>
+                            <option value="cliente">Cliente</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+            <!--Aqui va ir las tablas de reporte-->
+            <div class="col-md-8 border start border-5 border-dark">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
